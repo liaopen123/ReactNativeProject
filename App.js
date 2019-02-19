89620779f8ee926433199295809dc8396c12ba14
pony <467969@qq.com>
2019-02-19 22:50:29 +0800

    add  flatList

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
  * 我们使用StyleSheet.create 集中定义组件的样式
  */
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text,TextInput, ScrollView,View} from 'react-native';
+import {Platform, StyleSheet, Text,FlatList, ScrollView,View} from 'react-native';
 
 
 
@@ -14,18 +14,66 @@ export default class BlinkApp extends Component<Props> {
   }
  render(){
    return(
-     
-     <View style={{padding:20,backgroundColor:'white'}} >
-     <ScrollView showsVerticalScrollIndicator={false}>
-    <TextInput style={{height:40}} placeholder='这是占位符' 
-    onChangeText={(text)=>this.setState({text})}/>
-    <Text style={{padding:10,fontSize:100}}>
-      {this.state.text.split('').map((word)=>word&&':pizza:').join('')}
-    </Text>
-    </ScrollView>
+     <View  style={[style.container,backgroundcolor='white']}>
+     <FlatList data={[
+       {key:'lph'},
+       {key:'lph1'},
+       {key:'lph2'},
+       {key:'lph3'},
+       {key:'lph4'},
+       {key:'lph5'},
+       {key:'lph6'},
+       {key:'lph7'},
+       {key:'lph8'},
+       {key:'lph9'},
+       {key:'lph10'},
+       {key:'lph11'},
+       {key:'lph12'},
+       {key:'lph13'},
+       {key:'lph14'},
+       {key:'lph14'},
+       {key:'lph14'},
+       {key:'lph14'},
+       {key:'lph14'},
+       {key:'lph14'},
+       {key:'lph14'},
+       {key:'lph14'},
+       {key:'lph14'},
+       {key:'lph14'},
+       {key:'lph14'},
+       {key:'lph14'},
+       {key:'lph14'},
+       {key:'lph14'},
+       {key:'lph14'},
+       {key:'lph14'},
+        {key:'lph14'},
+       {key:'lph14'},
+       {key:'lph14'},
+       {key:'lph14'}, 
+        {key:'lph14'},
+       {key:'lph14'},
+       {key:'lph14'},
+       {key:'lph14'},
+       {key:'lph14'},
+       {key:'lph14'},  
+       {key:'lph14'},
+     ]}
+     renderItem={({item}) => <Text style={style.item}>{item.key}</Text>}/>
      </View>
 
    );
  }
 }
 
+const style =  StyleSheet.create({
+  container:{
+      flex:1,
+      paddingTop:22,
+      backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+  item:{
+    padding:10,
+    fontSize:18,
+    height:44,
+  }
+});
