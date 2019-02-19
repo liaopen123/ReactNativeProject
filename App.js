ee248481bb02bd149ab0fce8c5a010a0ee8e5e26
pony <467969@qq.com>
2019-02-19 22:18:05 +0800

    添加TextInput

--- App.js
+++ App.js
@@ -2,18 +2,24 @@
  * 我们使用StyleSheet.create 集中定义组件的样式
  */
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text,TextInput, View} from 'react-native';
 
 
 
 type Props = {};
 export default class BlinkApp extends Component<Props> {
+  constructor(props){
+    super(props);
+    this.state={text:''};
+  }
  render(){
    return(
-     <View style={{flex:1,flexDirection:'column',justifyContent:'center',alignItems:'stretch',backgroundColor:'white'}}>
-     <View style={{height:50,backgroundColor:'powderblue'}}></View>
-     <View style={{height:50,backgroundColor:'skyblue'}}></View>
-     <View style={{height:50,backgroundColor:'steelblue'}}></View>
+     <View style={{padding:20,backgroundColor:'white'}} >
+    <TextInput style={{height:40}} placeholder='这是占位符' 
+    onChangeText={(text)=>this.setState({text})}/>
+    <Text style={{padding:10,fontSize:40}}>
+      {this.state.text.split('').map((word)=>word&&':pizza:').join('')}
+    </Text>
      </View>
    );
  }
