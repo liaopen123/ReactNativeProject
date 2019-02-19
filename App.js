8f1357413b0394413de7b9f97387f23ccb7f5d58
pony <467969@qq.com>
2019-02-19 22:22:11 +0800

    add scrollview

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
  * 我们使用StyleSheet.create 集中定义组件的样式
  */
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text,TextInput, View} from 'react-native';
+import {Platform, StyleSheet, Text,TextInput, ScrollView,View} from 'react-native';
 
 
 
@@ -14,13 +14,17 @@ export default class BlinkApp extends Component<Props> {
   }
  render(){
    return(
+     
      <View style={{padding:20,backgroundColor:'white'}} >
+     <ScrollView showsVerticalScrollIndicator={false}>
     <TextInput style={{height:40}} placeholder='这是占位符' 
     onChangeText={(text)=>this.setState({text})}/>
-    <Text style={{padding:10,fontSize:40}}>
+    <Text style={{padding:10,fontSize:100}}>
       {this.state.text.split('').map((word)=>word&&':pizza:').join('')}
     </Text>
+    </ScrollView>
      </View>
+
    );
  }
 }
