0c79820931152bdb799601264f4cd5b00f59ad8b
pony <467969@qq.com>
2019-02-19 21:41:05 +0800

    添加justifyContent

--- App.js
+++ App.js
@@ -10,10 +10,10 @@ type Props = {};
 export default class BlinkApp extends Component<Props> {
  render(){
    return(
-     <View style={{flex:1,flexDirection:"row-reverse"}}>
-     <View style={{flex:1,backgroundColor:'powderblue'}}></View>
-     <View style={{flex:2,backgroundColor:'skyblue'}}></View>
-     <View style={{flex:3,backgroundColor:'steelblue'}}></View>
+     <View style={{flex:1,flexDirection:"column",justifyContent:'center',backgroundColor:'white'}}>
+     <View style={{width:50,height:50,backgroundColor:'powderblue'}}></View>
+     <View style={{width:50,height:50,backgroundColor:'skyblue'}}></View>
+     <View style={{width:50,height:50,backgroundColor:'steelblue'}}></View>
      </View>
    );
  }
